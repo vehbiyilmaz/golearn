a2248650f0ad1b625249ee8a6097b14ae1aa16b8
vehbiyilmaz <vvyilmaz@gmail.com>
2022-10-20 13:46:24 +0200

    updating some additional info to array.go

--- arrays.go
+++ arrays.go
@@ -6,14 +6,14 @@ import (
 
 func main(){
 
-	// array is a collection of elements of the same type placed contiguos in the memory 
-	// array has a fixed lenght . 
-
+// array is a collection of elements of the same type placed contiguos in the memory 
+// array has a fixed lenght. 
+	
 // Array DECLARATION 
 //   Name   |lenght | type |elements 
 var myArray = [3] int {1,2,3}
-
-//myArray := [3] int {0,1,2} /short way 
+// myArray := [5] int {1,2,3,4,5} // Short way with initialization 
+// myArray := [...] int {1,2,3,4,5,6} // lenght of array will be set automaticly by Complier
 
 // Different types of initializing an array 
 // var myArray = [3] int {} // declared not initiallized 
@@ -21,9 +21,30 @@ var myArray = [3] int {1,2,3}
 // var myArray = [3] int {0:0, 2:2} // index based initialization 
 
 // Array manipulation 
-myArray[0] = 1 // firt element changed tfrom 0 to  1 
+myArray[0] = 1 // firt element changed from 0 to  1 
 myArray = [3]int {10,20,30} // all elements are changed 
 
-fmt.Println(myArray)
+fmt.Println("manipulated array ", myArray)
 
+// IMPORTANT COPY ARRAYS 
+// If you make new array from existing array you are making just
+// new idependet copy of this array look below -->
+b:= myArray
+//change something usein "b" as you see Original Array is intakt 
+b[0] = 23
+fmt.Println("this is new array", b)
+fmt.Println("this is original  array", myArray)
+
+// POINTING Original array 
+origianlArray:= [3]int{1,2,3}
+
+// we use POINTER "&" to original Array we do not copy it we are just pointing to Original array 
+copyArray := &origianlArray
+
+//lets make some changes on COPY 
+copyArray[0] = 23
+
+// Lets see what happned to Original Array and Copy
+fmt.Println("Original Array > ", origianlArray)
+fmt.Println("Copy Array >", copyArray)
 }
